a62a3e309f6bcdda547660472389171e6e62d94a
abdWasiq <abdul.wasiq@naheed.pk>
2022-03-31 10:19:11 +0500

    project created

---
+++ touchdown/touchdown/touchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  touchdownApp.swift
+//  touchdown
+//
+//  Created by NaheedPK on 31/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct touchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
